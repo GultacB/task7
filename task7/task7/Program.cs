72b543971234b49f9efd09e2fbc18b236971f31f
GultacB <160045911+GultacB@users.noreply.github.com>
2024-02-23 10:55:01 +0400

    task7

---
+++ task7/task7/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = Console.ReadLine();
+int balance = 0;
+while (balance <= 10)
+{
+    if (input == "@@@" || input =="###" || input == "$$$")
+    {
+        Console.WriteLine(balance++);
+    }
+    else 
+    {
+        Console.WriteLine(balance--);
+        break;
+    }
+    
+}
